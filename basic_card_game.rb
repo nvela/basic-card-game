58c7a5b9edef830b1f699a9fd95e39349cdae2a2
Nick Vela <nickvela@Nicks-MacBook-Pro.local>
2013-10-05 15:39:07 -0700

    Added new code into basic_card_game file and added resource_code file for reference.

--- basic_card_game.rb
+++ basic_card_game.rb
@@ -1,7 +1,11 @@
-# this program should display a hand of 7 random cards from a deck of 52 standard playing cards.
+# phase 1: this program should display a hand of 7 random cards from a deck of 52 standard playing cards.
 
-suits = %w[spades hearts diamonds clubs]
-ranks = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-deck = suits + ranks
+rank = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+suit = %w[Club Diamond Heart Spade]
 
-puts deck.shuffle
+deck = (rank + suit).shuffle
+
+# need to figure out to pull 7 randomized cards from the deck.
+hand = deck  	
+  
+puts hand
